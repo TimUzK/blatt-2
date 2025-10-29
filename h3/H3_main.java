143d64adce38805643dd7ea0dc7dab9482c62117
TimUzK <tbersch@smail.uni-koeln.de>
2025-10-29 17:21:26 +0100

    Hausaufgabe 3

--- h3/H3_main.java
+++ h3/H3_main.java
@@ -1,3 +1,25 @@
 public class H3_main {
+    public static void main(String[] args) {
+        int i = 205;
+        int j = 5;
+        int k = -10;
+
+        if (i > j && !(i > 200) && !(j > 100)) {
+            k = 1;
+        }
+
+        else if (i > j && i > 200 && !(j > 100)) {
+            k = 2;
+        }
+
+        else if (i > j && i > 200 && j > 100) {
+            k = 3;
+        }
+
+        else if (!(i > j) && !(i > 200) && !(j > 100)) {
+            k = 4;
+        }
+
+    }
 
 }
