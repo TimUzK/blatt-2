0a85aebb21eb4eee75341b424ce7a18cf981c22e
TimUzK <tbersch@smail.uni-koeln.de>
2025-10-29 15:22:33 +0100

    Präsenz 3

--- p2/P2_main.java
+++ p2/P2_main.java
@@ -1,13 +1,12 @@
-public class P2_main {  
+public class P2_main {
     public static void main(String[] args) {
 
-    int i=10;
-    boolean b=false;
+        int i = 11;
+        boolean b = false;
 
-    if (i>10) {
-        b = true;
-    }
-    System.out.println(b);
+        if (i > 10) {
+            b = true;
+        }
 
     }
 }
---
+++ p3/P3_main.java
@@ -0,0 +1,20 @@
+public class P3_main {
+    public static void main(String[] args) {
+        int i = 5;
+        int j = 10;
+        int k = 15;
+
+        if (i < j) {
+            System.out.println("A");
+        }
+
+        if (i < k) {
+            System.out.println("B");
+        }
+
+        if (j < k) {
+            System.out.println("C");
+        }
+    }
+
+}
