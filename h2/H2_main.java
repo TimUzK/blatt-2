818f2e1cd19238028e786925e603288a3ed5ed69
TimUzK <tbersch@smail.uni-koeln.de>
2025-10-29 16:34:24 +0100

    Hausaufgabe 2

---
+++ h2/H2_main.java
@@ -0,0 +1,31 @@
+public class H2_main {
+    public static void main(String[] args) {
+        int i=5;
+        int j=10;
+        int k=15;
+        int min=1;
+        int max=100;
+
+        min= i;
+        max= i;
+        
+        if (j<i && j<k) {
+            min=j;
+        }
+
+        if (k<i && k<j) {
+            min=k;
+        }
+
+        if (j>i && j>k) {
+            max=j;
+        }
+        
+        if (k>i && k>j) {
+            max=k;
+        }
+
+        System.out.println("min: " + min);
+        System.out.println("max: " + max);
+    }
+}
