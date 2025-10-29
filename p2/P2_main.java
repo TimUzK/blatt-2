3543d9ab2356011d06e043d48a2e4ba5c30c5a1a
TimUzK <tbersch@smail.uni-koeln.de>
2025-10-29 15:12:53 +0100

    Präsenz 2

---
+++ p2/P2_main.java
@@ -0,0 +1,13 @@
+public class P2_main {  
+    public static void main(String[] args) {
+
+    int i=10;
+    boolean b=false;
+
+    if (i>10) {
+        b = true;
+    }
+    System.out.println(b);
+
+    }
+}
